8deda5b882ac2b051a585d2f337a3092cdb8a098
Helmkampmc <120995077+Helmkampmc@users.noreply.github.com>
2023-03-26 00:26:37 -0400

    Update testing_analysis.py

--- testing_analysis.py
+++ testing_analysis.py
@@ -67,7 +67,7 @@ df4=df4.nlargest(1, '2021 Crimes')
 filtered_df1=df1[df1["ORI - Agency"]==ori_selection]
 filtered_df1=filtered_df1.nlargest(1, '2019 Crimes')
 df4['2019 Crimes']=filtered_df1['2019 Crimes'].astype('int64')
-st.write(df4)
+st.dataframe(df4)
 
 # Create a line chart using Plotly Express
 
